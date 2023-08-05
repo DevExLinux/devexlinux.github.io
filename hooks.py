be0f280d7cac71f9403922d398a96060fb7d6c3a
João Pinto <lamego.pinto@gmail.com>
2023-08-05 11:27:45 +0200

    Prepare the linux profile

--- hooks.py
+++ hooks.py
@@ -4,4 +4,5 @@ import shutil
 
 def on_post_build(config, **kwargs):
     site_dir = config['site_dir']
-    shutil.copy('assets/install.ps1', os.path.join(site_dir, 'install'))
+    shutil.copy('assets/install.ps1', os.path.join(site_dir))
+    shutil.copy('assets/install.sh', os.path.join(site_dir))

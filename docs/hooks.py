279b24fb9121c4bba81fc0c52a89dc0510a4681a
João Pinto <lamego.pinto@gmail.com>
2023-08-05 04:00:56 +0200

    Fix the hooks plugin

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,6 +1,6 @@
 import os
 import shutil
 
-def copy_get(config, **kwargs):
+def on_post_build(config, **kwargs):
     site_dir = config['site_dir']
     shutil.copy('assets/install.ps1', os.path.join(site_dir, 'install'))
